ed80fb160844a05dac154056607e5d408a2ec935
StarUnu <etamotu@gmail.com>
2017-08-05 14:21:53 -0500

    añadiendo proyecto prueba

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Calendari Javascript</title>
+	<script  src="main.js" defer></script>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+</body>
+</html>
---
+++ nuevo.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Nuevo</title>
+</head>
+<body>
+
+</body>
+</html>
